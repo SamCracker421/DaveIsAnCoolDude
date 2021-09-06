55aba52fa7f632e6feb82ecbc2464e6016d5bca8
GrahamCracker421 <Pleasedontspamme421@gmail.com>
2021-09-06 06:47:52 -0400

    It has flexbox :wink:

--- Homework2/alertthingy.js
+++ Homework2/alertthingy.js
@@ -1,8 +1,7 @@
 function cheese2(){
-
 function cheese1(){
 var x= Math.round(((Math.random()*100)+10),4);
 alert("Your Cheese Order Is Currently: "+x+"$");
 }
-setInterval(cheese1,2500);
 }
+setInterval(cheese1,2500);
