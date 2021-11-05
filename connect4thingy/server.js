1aa8333c92f3dc55a57cde6b4f037a4fc241408a
Sam Blodgett <Pleasedontspamme421@gmail.com>
2021-11-04 21:20:06 -0400

    suck

--- connect4thingy/server.js
+++ connect4thingy/server.js
@@ -10,48 +10,60 @@ app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 10*1000 }}))
 
 const path = require('path');
 const { send } = require('process');
+const { brotliDecompressSync } = require('zlib');
 app.use('/', express.static(path.join(__dirname, 'static')));
 let players=[];
-let array=[
-    [2, 0, 0, 1,0, 2, 0],
-    [0, 0, 0, 0,0, 0, 0],
-    [0, 0, 1, 0,2, 0, 0],
-    [0, 0, 0, 0,0, 0, 0],
-    [2, 0, 0, 0,2, 0, 0],
-    [0, 0, 2, 0,0, 0, 0]
-  ]
+let array=createboard();
 let turn=0;
 
-// function createboard(){
-//     let column=[];
-//     for(let x=0; x<6;x++){
-//         let row=[];
-//         for(let y=0; y<7;y++){
-//             row.push(0);
-//         }
-//         column.push(row);
-//     }
-//     return column;
-// }
+function createboard(){
+    let row=[];
+    for(let x=0; x<6;x++){
+        let column=[];
+        for(let y=0; y<7;y++){
+            column.push(0);
+        }
+        row.push(column);
+    }
+    return row;
+}
 
-// function startup(){
-//     let array=createboard();
-//     let playerturn=1;
-//     let player={
-//         "id":nanoid(),
-//         "turn":false
-//     }
-//     players.push(player);
-// }
+function startup(){
+    let array=createboard();
+    let playerturn=1;
+    let player={
+        "id":nanoid(),
+        "turn":false
+    }
+    players.push(player);
+}
+let gamestate={
+    "array":array,
+    "players":players,
+    "turn":turn
+}
+
+app.post('')
 app.get('/game', (req,res) =>{
+
 res.json(array);
 })
 app.post('/game',(req,res)=>{
-
 res.json(array);
-
 })
-// console.log(createboard());
+
+app.post('/move',(req,res)=>{
+let key=req.body.key-1;
+if(key!=-1){
+    for(let x=0; x<array.length-1;x++){
+        if((array[x][key]===0) && (((array[x+1][key])!=0) || x===array.length-2)){
+            array[x][key]=2;
+        }
+        console.log(key);
+    }
+}   
+    
+})
 app.listen(port, () => {
     console.log(`BlackJack Server at http://localhost:${port}`)
 });
--- connect4thingy/static/connect4.js
+++ connect4thingy/static/connect4.js
@@ -1,42 +1,96 @@
-let board;
-function getEvents(){
-    fetch(`/game`, {
+document.addEventListener("keydown", cheedar);
+let once=true;
+document.addEventListener("keyup",()=>{once=true})
+async function getEvents() {
+    return await fetch('/game', {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json',}
-            
-        })
+            'Content-Type': 'application/json',
+        }
+    })
         .then(res => res.json())
         .then(data => {
-            board=data;
+            console.log(data)
+            return data;
         })
         .catch(error => {
             console.log(error);
         })
 }
-getEvents();
+function cheedar(e) {
+    function key(){
+    once=false;
+        switch (e.which) {
+        case 49:
+            return 1;
+        case 50:
+            return 2;
+        case 51:
+            return 3;
 
-  let c = document.getElementById("canvasthing");
-  c.width = 1000;
-  c.height = 900;
-  let context = c.getContext("2d");
-console.log(board);
-  function cheese() {
-    for (let row = 0; row < 6; row++) {
-      for (let column = 0; column < 7; column++) {
-        context.beginPath()
-        x = 100 * column + 50;
-        y = 100 * row + 50;
-        context.arc(x, y, 50, 0, Math.PI * 2);
-        if (board[row][column] == 1) {
-          context.style = 'red';
-        } else if (board[row][column] == 2) {
-          context.style = 'yellow';
-        } else {
-          context.style = 'black';
+        case 52:
+            return 4;
+
+        case 53:
+            return 5;
+
+        case 54:
+            return 6;
+
+        case 55:
+            return 7;
+
+        default:
+            return 0;
+    }}
+    if(once===true){
+    fetch('/move', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
         }
-        context.stroke();
-      }
-    }
-  }
-  cheese();
+        ,
+            body: JSON.stringify({
+                "key":key()
+            }
+                )
+    })
+        .then(res => res.json())
+        .then(data => {
+        })
+        .catch(error => {
+            console.log(error);
+        })}
+        else{
+            console.log(intermediate);
+        }
+}
+
+let c = document.getElementById("canvasthing");
+c.width = 700;
+c.height = 600;
+let context = c.getContext("2d");
+
+function cheese() {
+    getEvents().then(iHateKneeGrows => {
+        for (let row = 0; row < 6; row++) {
+            for (let column = 0; column < 7; column++) {
+                context.beginPath()
+                x = 100 * column + 50;
+                y = 100 * row + 50;
+                context.arc(x, y, 50, 0, Math.PI * 2);
+                if (iHateKneeGrows[row][column] == 1) {
+                    context.fillStyle = 'red';
+                } else if (iHateKneeGrows[row][column] == 2) {
+                    context.fillStyle = 'yellow';
+                } else {
+                    context.fillStyle = 'black';
+                }
+                context.fill()
+                context.stroke();
+            }
+        }
+    })
+}
+
+setInterval(cheese,1000);
