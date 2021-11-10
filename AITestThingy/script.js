e343b30ecf591d624b5cdb52f58b548626b0afe8
Sam Blodgett <Pleasedontspamme421@gmail.com>
2021-11-09 22:45:25 -0500

    idk

--- AITestThingy/script.js
+++ AITestThingy/script.js
@@ -1,7 +1,59 @@
 let con=document.getElementById("canvasthingy");
 let ctx=con.getContext('2d');
+let pos={x:100,y:100};
+let velocity={x:0,y:2};
+let acc={x:0,y:0.4};
+let death=false;
+let blocks=[];
+
+document.addEventListener('keydown',function (e) {
+    if(e.which==38){
+        pos.y+=-30
+        velocity.y=2;
+        console.log("up");
+    }
+});
+function checkdeath(){
+    if(pos.y>700 || pos.y<0){
+        death=true;
+    }
+}
+function createboxes(){
+    let blockwithpos=[];
+    let blockset=[];
+    this.height=Math.floor(Math.random()*300)+10;
+    blockset.push(height);
+    this.height=600-blockset[0];
+    blockset.push(height);
+    blockwithpos.push(blockset);
+    let position={x:500};
+    blockwithpos.push(position);
+    blocks.push(blockwithpos);
+}
+function drawboxes(){
+    for(let i=0; i<blocks.length;i++){
+    ctx.rect(blocks[0]);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+    }
+}
+
+function draw(){
+checkdeath();
+if(death===false){
 ctx.beginPath();
-ctx.rect(100,100,50,50);
+ctx.clearRect(0, 0, 500, 700);
+ctx.rect(pos.x,pos.y,50,50);
 ctx.fillStyle="red";
 ctx.fill();
+pos.y+=velocity.y;
+velocity.y+=acc.y;
+ctx.stroke();
+}
+else{
+    let cheese=document.createElement('h1');
+    cheese.innerText="You have Died";
+    document.getElementById("cheddar").appendChild(cheese);
+}
+}
+setInterval(draw,1000/30);
 
+ 
--- SignOfGod/static/SIgnOfGod.js
+++ SignOfGod/static/SIgnOfGod.js
@@ -1,7 +1,6 @@
-let students;
-let events=[];
-function getStudent(id) {
-    fetch(id==undefined ? `/students` : `/students/${id}`, {
+
+async function getStudent(id) {
+    return await fetch(id==undefined ? `/students` : `/students/${id}`, {
     method: 'GET',
     headers: {
         'Content-Type': 'application/json',
@@ -9,44 +8,44 @@ function getStudent(id) {
     })
     .then(res => res.json())
     .then(data => {
-        console.log(data);
-        students=data;
+        return data;
     })
     .catch(error => {
         console.log("error")
     })
 }
-function getEvents(){
-    fetch(`/events`, {
+async function getEvents(){
+return await fetch(`/events`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',}
         })
         .then(res => res.json())
         .then(data => {
-            events=data;
+            return data;
         })
         .catch(error => {
             console.log(error);
-        })
-}
-function getEvent(id) {
-    fetch(id==undefined ? `/events` : `/events/${id}`,{
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            events=data;
-        })
-        .catch(error => {
-            console.log(error)
-        })
-        
-}
+        });
+    }
+
+    function getEvent(id) {
+        fetch(id==undefined ? `/events` : `/events/${id}`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                events=data;
+            })
+            .catch(error => {
+                console.log(error)
+            })
+            
+    }
 function addstudent(firstname, lastname){
     fetch(`/students`, {
         method: 'POST',
@@ -61,6 +60,7 @@ function addstudent(firstname, lastname){
         .then(res => res.json())
         .then(data => {
             console.log(data);
+            
         })
         .catch(error => {
             console.log("error")
@@ -125,4 +125,70 @@ function createsignup(userId,eventId,signout){
             console.log("error")
         })
     }
-    
+async function getstudentevents(id) {
+   return await fetch(`/students/${id}/events`,{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+        })
+        .then(res => res.json())
+        .then(data => {
+            return data;
+        })
+        .catch(error => {
+            console.log(error)
+        })
+        
+}
+    function something(){
+        console.log("calling");
+        getEvents().then(events =>{
+            console.log(events)
+        for(let pp=0; pp<events.length;pp++){
+            let cormdog=document.createElement('input');
+            cormdog.type='radio';
+            cormdog.id=events[pp].name;
+            cormdog.class="events";
+            cormdog.name="events";
+            cormdog.value=events[pp].id;
+            let cheese=document.createElement('label');
+            cheese.for=cormdog.id;
+            cheese.innerText="Name: " + events[pp].name + " || Date: "+ events[pp].date + " || Time: " + events[pp].time;
+
+
+            let fat=document.createElement("br");
+
+            document.getElementById('radioshack').appendChild(cheese);
+            document.getElementById('radioshack').appendChild(cormdog);
+            document.getElementById('radioshack').appendChild(fat);
+            }
+        })
+
+        }
+    function cheetoes(potato){
+        let lastid;
+        getStudent().then(students=>{
+        console.log(potato);
+        let chew=students[students.length-1];
+        console.log("chew: "+chew);
+        let dt=new Date();
+        let hours=dt.getHours();
+        let seconds=dt.getSeconds();
+        createsignup(chew.id,potato,hours + ":"+ seconds);
+        lastid=chew;
+        });
+        document.body.innerHTML="";
+        let wall=document.createElement("div");
+        let btn1=document.createElement("button");
+        let text=document.createElement("h");
+        wall.id="wall";
+        btn1.innerHTML="Click Here to go back";
+        btn1.onclick=()=>{
+            window.open("http://192.168.3.29:6969/");
+        }
+        document.body.appendChild(wall);
+        document.getElementById("wall").appendChild(text);
+        document.getElementById("wall").appendChild(btn1);
+
+    }
